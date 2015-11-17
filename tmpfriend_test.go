111fd541b5458c87bde553a0511ae9101d9604b5
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2015-11-17 15:49:27 +0200

    Test RootTempDir

--- tmpfriend_test.go
+++ tmpfriend_test.go
@@ -1,6 +1,10 @@
 package tmpfriend
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestIsTmpFriendDir(t *testing.T) {
 	valid := []string{"tmpfriend-1-", "tmpfriend-123-123", "/tmp/tmpfriend-123-3"}
@@ -14,5 +18,37 @@ func TestIsTmpFriendDir(t *testing.T) {
 		if IsTmpFriendDir(d) {
 			t.Errorf("%v is an invalid tmpfriend dir", d)
 		}
+	}
+}
+
+func TestRootTempDir(t *testing.T) {
+	d, err := ioutil.TempDir("", "testroottempdir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(d)
+
+	old := os.Getenv("TMPDIR")
+	cleanup, err := RootTempDir(d)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testDir, err := ioutil.TempDir("", "testroottempsubdir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.Stat(testDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cleanup()
+	_, err = os.Stat(testDir)
+	if err == nil || !os.IsNotExist(err) {
+		os.RemoveAll(testDir)
+		t.Errorf("Cleanup did not delete sub temp dir %v", testDir)
+	}
+	if os.Getenv("TMPDIR") != old {
+		t.Error("Failed to restore TMPDIR")
 	}
 }
